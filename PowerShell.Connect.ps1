d72502ca6e9418b3617fff6e8a7ea81b9b08df61
synopsis <synopsis@outlook.com>
2019-10-07 01:13:04 +0200

    .

---
+++ PowerShell.Connect.ps1
@@ -0,0 +1,9 @@
+﻿$prData = Get-Content -LiteralPath "C:\Lab\FUJ-D-WE-LAB100-RG.txt" | Out-String | ConvertFrom-Json
+
+Set-Clipboard -Value $prData.Secret
+
+$Credential = Get-Credential -UserName $prData.ApplicationId -Message "Provide Password"
+
+Connect-AzAccount -ServicePrincipal -Credential $Credential -Tenant $prData.TenantId -Subscription $prData.SubscriptionId
+
+Get-AzResourceGroup
